158b5c036abf592b13c45fbf035769aba172b5e3
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-10-25 16:55:50 +1030

    Added some simple movement to demo

---
+++ Assets/VRCameraSimple.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRCameraSimple : MonoBehaviour {
+
+    public GameObject indicator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 fwd = transform.forward;
+        fwd.y = 0;
+        bool lookingDown = false;
+        if (transform.eulerAngles.x > 25)
+        {
+            lookingDown = true;
+            transform.parent.position = transform.parent.position + fwd * 0.3f;
+        }
+
+        float scale = lookingDown ? 0.5f : 1.0f;
+        indicator.transform.localScale = new Vector3(scale, scale, scale);
+
+        indicator.transform.rotation = transform.rotation;
+	}
+}
